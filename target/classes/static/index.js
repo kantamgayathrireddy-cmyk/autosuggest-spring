ed6c44016787f701b8b0e1440dba6b456bdb460c
K.Gayathri <kantamgayathrireddy@gmail.com>
2026-07-18 14:34:22 +0530

    files added

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,29 +1,31 @@
-// alert("hello from js")
-// console.log("hello from js")
-var users=[
+// alert("Hello guysss");
+
+console.log("hello from JS!")
+
+var users = [
     {
-        "name":"John Doe",
-        "gender":"Male",
-        "img" :"john.png"
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
     },
     {
-        "name":"Jane Doe",
-        "gender":"Female",
-        "img" :"jane.png"
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
     }
 ]
-var id=0;
 
-function toggleUser()
-{
-    id=(id+1)%2;
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
     // get the elements
 
-    // 1:image
+    // 1: image
     var userImage = document.getElementById("user-image");
     userImage.src = users[id].img;
 
-    // 2:name
+    // 2: name
     var userName = document.getElementById("user-name");
     userName.innerHTML = users[id].name;
 
