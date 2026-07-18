ee18fb7177deac0cbc91cf18790ad07cc1f94258
K.Gayathri <kantamgayathrireddy@gmail.com>
2026-07-18 14:23:27 +0530

    files added

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,29 +1,31 @@
-// alert("hello from js")
-// console.log("hello from js")
-var users=[
+// alert("Hello guysss");
+
+console.log("hello from JS!")
+
+var users = [
     {
-        "name":"John Doe",
-        "gender":"Male",
-        "img" :"john.png"
+        "name" : "John Doe",
+        "gender" : "Male",
+        "img" : "john.png"
     },
     {
-        "name":"Jane Doe",
-        "gender":"Female",
-        "img" :"jane.png"
+        "name" : "Jane Doe",
+        "gender" : "Female",
+        "img" : "jane.png"
     }
 ]
-var id=0;
 
-function toggleUser()
-{
-    id=(id+1)%2;
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
     // get the elements
 
-    // 1:image
+    // 1: image
     var userImage = document.getElementById("user-image");
     userImage.src = users[id].img;
 
-    // 2:name
+    // 2: name
     var userName = document.getElementById("user-name");
     userName.innerHTML = users[id].name;
 
@@ -31,33 +33,4 @@ function toggleUser()
     var userGender = document.getElementById("user-gender");
     userGender.innerHTML = users[id].gender;
 
-}
-function randomUser(){
-    fetch('https://randomuser.me/api/')
-        .then(function(response){
-            // convert raw text to json
-            return response.json();
-        })
-        // dom mannipulation with API data
-        .then(function(data){
-
-            var userData = data.results[0];
-
-            // 1: image
-            var userImage = document.getElementById("user-image");
-            userImage.src = userData.picture.large;
-
-            // 2: name
-            var userName = document.getElementById("user-name");
-            userName.innerHTML = userData.name.first + " " +
-                                    userData.name.last;
-
-            // 3: gender
-            var userGender = document.getElementById("user-gender");
-            userGender.innerHTML = userData.gender;
-        })
-        // handle error
-        .catch(function(err){
-            console.log(err);
-        })
 }
